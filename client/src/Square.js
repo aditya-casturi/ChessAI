130684e9af5409aee3c1b2d2076dee62b576d630
aditya <adityac24@lakesideschool.org>
2023-02-18 23:28:08 -0800

    Format

--- client/src/Square.js
+++ client/src/Square.js
@@ -24,7 +24,15 @@ export default function Square(props) {
             ${props.squareColor} 
             ${props.selected ? 'selected-square' : ''}
         `} onClick={() => props.handleSquareClick(props.squareIndex)}>
-            <div draggable id={props.piece + props.squareIndex} onMouseDown={() => props.handleSquareClick(props.squareIndex)} onDragStart={(event) => handleDragStart(event, props)} onDragOver={handleDragOver} onDrop={handleDrop} style={{zIndex: '1', position: 'relative'}} className={`piece ${props.piece}`}>
+            <div 
+                draggable 
+                id={props.piece + props.squareIndex}
+                onMouseDown={() => props.handleSquareClick(props.squareIndex)}
+                onDragStart={(event) => handleDragStart(event, props)}
+                onDragOver={handleDragOver}
+                onDrop={handleDrop}
+                style={{zIndex: '1', position: 'relative'}} className={`piece ${props.piece}`}>
+
                 <div id={props.squareIndex} className='dot' style={{visibility: props.legalSquares.includes(props.squareIndex) ? 'visible' : 'hidden' }}>●</div>
             </div>
         </div>
