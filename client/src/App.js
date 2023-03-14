40aede579202f0abb99e1ba39b661baebc713d52
Daniel Zhu <daniel_zhu25@outlook.com>
2023-03-13 22:27:19 -0700

    added a basic navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import Chessboard from './Chessboard'
+import './App.css'
 
 export default function App() {
   return (
+    <div>
       <Chessboard />
+      <html>
+        <head></head>
+        <body>
+          <div class="topnav">
+            <a href="home">Home</a>
+            <a class="active" href="play">Play</a>
+            <a href="contact">Contact</a>
+            <a href="about">About</a>
+          </div>
+          <div class="topnav1">
+            <a href="logout">Logout</a>
+          </div>
+        </body>
+      </html>
+    </div>  
   )
 }
